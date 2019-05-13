9005937f15f1f4df8ab4b02154cf6e78969fd103
Mubashir Zainoor <mub_noor@yahoo.com>
2019-05-13 06:31:37 +0500

    Update plotData.m

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -3,10 +3,10 @@ function plotData(x, y)
 figure; % open a new figure window
 
 data = load('ex1data1.txt'); 
-x = data(:, 1); 
+X = data(:, 1); 
 y = data(:, 2); 
 m = length(y);
-plot(x, y, 'rx', 'MarkerSize', 10); 
+plot(X, y, 'rx', 'MarkerSize', 10); 
 ylabel('Profit in $10,000s'); 
 xlabel('Population of City in 10,000s');
 
